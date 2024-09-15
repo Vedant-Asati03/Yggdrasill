0f5bc97f815fa6df382728394f97091ddc94c32e
vedant-asati03 <vedant.asati03@gmail.com>
2024-09-16 01:27:11 +0530

    a tui created using textual.

--- yggdrasill.py
+++ yggdrasill.py
@@ -2,36 +2,18 @@ import json
 import argparse
 
 from pathlib import Path
-from rich.tree import Tree
 from rich.console import Console
+from textual.app import App, ComposeResult
+from textual.widgets import DirectoryTree
 
 
-console = Console()
+class DirectoryTreeApp(App):
+    def __init__(self, directory: Path):
+        self.directory = directory
+        super().__init__()
 
-
-def distinct_file_color(file):
-
-    with open("file_extensions.json", "r") as extension_file:
-        file_extensions: dict = json.load(extension_file)
-
-    for extension in file_extensions:
-        if file.endswith(extension):
-            return file_extensions.get(extension)
-        else:
-            return "[#FFFFFF]"
-
-
-def generate_tree(directory: Path, tree: Tree):
-
-    contents = sorted([p for p in directory.iterdir()])
-
-    for path in contents:
-        if path.is_dir():
-            branch = tree.add(f"[#6D43A6]{path.name}[/]")
-            generate_tree(path, branch)
-        else:
-            color = distinct_file_color(path.name)
-            tree.add(f"{color}{path.name}[/]")
+    def compose(self) -> ComposeResult:
+        yield DirectoryTree(self.directory)
 
 
 def main():
@@ -42,12 +24,13 @@ def main():
     args = parser.parse_args()
 
     directory = args.directory
+
+    app = DirectoryTreeApp(directory)
+
     if directory.is_dir():
-        tree = Tree(f"[#ABE15B]{directory}[/]", guide_style="underline2")
-        generate_tree(directory, tree)
-        console.print(tree)
+        app.run()
     else:
-        console.print("[#FFC835]The provided path is not a valid directory.[/]")
+        Console().print("[#FFC835]The provided path is not a valid directory.[/]")
 
 
 if __name__ == "__main__":
